5908840bb8c1638a24b97c06674bff62594b3ba1
ThothRex <thothrex@gmail.com>
2014-07-14 16:20:36 +0100

    Migrate scores.php to PDO & add error check

--- scores.php
+++ scores.php
@@ -7,19 +7,18 @@ if (!isset($_SESSION['username'])) {
     exit;
 } 
 
-$db_server = db_connect();               
+$dbh = db_connect();
 $query = "SELECT UserName AS username, 
-                 Wins AS wins,
-                 Defeats AS defeats
-          FROM Players      
+                 Wins     AS wins,
+                 Defeats  AS defeats
+          FROM  Players
           WHERE UserName <> 'System'    
           ORDER BY Wins-Defeats DESC, Wins DESC
           LIMIT 20";
-$result = $db_server->query($query);
-if ($result) {
-    echo sqlresult_to_json($result);
-    $result->free();
-}     
+$result = $dbh->query($query);
 
-$db_server->close();
+if ($result) echo sqlresult_to_json($result, PDO::FETCH_ASSOC);
+else throw new Exception("No non-system players");
+
+$dbh = null; //close connection
 ?>
