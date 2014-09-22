86198448171dd0404e06b5b24e28c3b48653f7c6
ThothRex <thothrex@gmail.com>
2014-09-22 16:47:19 +0100

    Update register.php to use PDO & Exceptions

--- register.php
+++ register.php
@@ -5,29 +5,20 @@ require_once('config.php');
 if (isset($_POST['username'])
 &&  isset($_POST['password'])
 &&  isset($_POST['email'])) {
-    $username = $_POST['username'];
+    $username       = $_POST['username'];
     $encrypted_pass = password_hash($_POST['password'], PASSWORD_DEFAULT);
 
-    $db_server = db_connect();
-    $statement
-        = $db_server->prepare("INSERT INTO Players(UserName,PwdHash,Email)
-                               VALUES(?,?,?)");
-    $statement->bind_param('sss',
-                           $username,
-                           $encrypted_pass,
-                           $_POST['email']);
-    if (!( $statement->execute() )) {
-        $registerError = $db_server->error;
-        error_log("\r\nregister.php error: $registerError", 3, 'debug.log');
-        echo "false";
-    }
-    else {
-        $_SESSION['username'] = $username;
-        echo "true";
-    }
+    $dbh = db_connect();
+    $sth = $dbh->prepare(
+        "INSERT INTO Players(UserName, PwdHash, Email)
+         VALUES             (?,        ?,       ?    )"
+    );
+    $sth->execute([$username, $encrypted_pass, $_POST['email']]);
 
-    $statement->close();
-    $db_server->close();
+    // if no exception thrown
+    $_SESSION['username'] = $username;
+    echo "true";
+    $db_server = null; //close connection
 }
 
 ?>
