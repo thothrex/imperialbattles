c315116ced3d5c0c7908bfc79845b39de9a3e2c4
thothrex <thothrex@gmail.com>
2017-11-15 16:36:14 +0000

    Fix incorrect message field name; code cleanup

--- js/chat.js
+++ js/chat.js
@@ -1,54 +1,36 @@
 /* MODEL */
 
-var lastfetched;
+var lastfetched = null;
 var updating = false;
 var updateInterval;
+var chatServerScriptUrl = "chat.php";
+var chatUpdateInterval = 1000; // In milliseconds
 
 function enableChatUpdate() {
-    clearInterval(updateInterval);
-    lastfetched = null;
-    $.getJSON("chat.php",
+    $.getJSON(chatServerScriptUrl,
         {  
             'function': 'fetch',
 			'gameid': game.gameid
         },
         function(messages){
-            if (messages != null){
-                displayChat(messages);
-                if (messages.length>0){
-                    lastfetched =  messages[messages.length-1].time;
-                }
-            }
-            else {
-                throw new Error("Error on fetching messages");
-            }
-            
-            updateInterval = setInterval('updateChat()', 1000);
+            updateChat(messages);
+            updateInterval = setInterval('requestChatUpdate()', chatUpdateInterval);
         }
     );
 }
 
-function updateChat() {
-    if (!updating){
-        updating = true;
-        $.getJSON("chat.php",
-            {
-                'function' : 'update',
-                'timestamp': lastfetched,
-                'gameid'   : game.gameid,
-                cache      : false
-            },
-            function(messages){
-                if (messages != null){
-                    if (messages.length>0){
-                        lastfetched = messages[messages.length-1].time;
-                        displayMessages(messages);
-                    }
-                }
-                updating = false;
-            }
-        );
-    }
+function requestChatUpdate() {
+    $.getJSON(chatServerScriptUrl,
+        {
+            'function' : 'update',
+            'timestamp': lastfetched,
+            'gameid'   : game.gameid,
+            cache      : false
+        },
+        function(messages){
+            updateChat(messages);
+        }
+    );
 }
 
 function sendMessage() {
@@ -63,55 +45,55 @@ function sendMessage() {
         },
         function(data) {
             if (data!=null) {
-                updateChat();
+                requestChatUpdate();
                 moveScrollbarToBottom();
             }
             else {
                 alert("Error on sending message");
+                throw "sendMessage received null server response.";
             }
         });
     }
 }
 
-
+function updateChat (messages) {
+    if (messages != null && messages.length > 0){
+        lastfetched = messages[messages.length-1].time;
+        displayMessages(messages);
+    }
+}
 
 
 /* VIEW */
 
-function displayChat(messages){
-    $('#chat-area').html("");
-    $.each(messages, function(i, field){
-        var parts = field.time.split(/[\s:-]+/);
-        appendChat("<b>" + field.username 
-                      + "(" + parts[3] + ":" + parts[4] + "): </b>"
-                      + field.message, field.username);                 
-        });
-        moveScrollbarToBottom();
-}
-
 function displayMessages(messages){
     var position = $('#chat-area')[0].scrollTop;
-    var scroll = position + $('#chat-area').height() + 40 
-                        > $('#chat-area')[0].scrollHeight;
+    var scroll
+        = position + $('#chat-area').height() + 40
+        > $('#chat-area')[0].scrollHeight;
+
     $.each(messages, function(i, field){
         var parts = field.time.split(/[\s:-]+/);
-        appendChat("<b>" + field.username 
-                      + "(" + parts[3] + ":" + parts[4] + "): </b>"
-                      + field.messageText,field.username);
-        });
-    if (scroll)
-        moveScrollbarToBottom();
-    else
-        $('#chat-area').scrollTop(position);
+        appendMessage(
+              parts[3] + ":" + parts[4] + " " // hours, minutes
+            + "<b>" + field.username + ":</b>"
+            + field.message
+            , field.username
+        );
+    });
+
+    if (scroll) { moveScrollbarToBottom(); }
+    else        { $('#chat-area').scrollTop(position); }
 }
 
-function appendChat(message,username) {
+function appendMessage(message,username) {
     var i;
     var width = $('#chat-area').width()/8;
     var p;
     if (username == "System") {
         p = "<p class='red'>";
-    } else {
+    }
+    else {
         p = "<p class='normal'>";
     }
     for(i = 0; i < message.length - width; i += width){
@@ -124,3 +106,9 @@ function moveScrollbarToBottom() {
     $('#chat-area').scrollTop($('#chat-area')[0].scrollHeight);
 }
 
+function displayError (error) {
+    console.log(
+        error.name + " @ " + error.fileName + error.lineNumber
+        + ": " + error.message
+    );
+}
